31f0fc79c7db71ee7951b64658eaba4c5b68c32b
kagasu <admin@kagasu.net>
2015-02-01 16:51:24 +0900

    first commit

---
+++ SoundCloudLikeDownloader/Downloader.cs
@@ -0,0 +1,72 @@
+﻿using Codeplex.Data;
+using SoundCloud.NET;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SoundCloudLikeDownloader
+{
+    class Downloader
+    {
+        private string clientId;
+        private string accessToken;
+
+        public Downloader(string clientID, string accessToken)
+        {
+            this.clientId = clientID;
+            this.accessToken = accessToken;
+        }
+
+        private static string ValidFileName(string s)
+        {
+            string valid = s;
+            char[] invalidch = System.IO.Path.GetInvalidFileNameChars();
+
+            foreach (char c in invalidch)
+            {
+                valid = valid.Replace(c, '_');
+            }
+            return valid;
+        }
+
+        private string GetMp3Url(int trackId)
+        {
+            var wc = new WebClient();
+            wc.Headers.Add("Authorization: OAuth " + this.accessToken);
+            wc.Headers.Add("Accept: application/json");
+            var str = wc.DownloadString("https://api.soundcloud.com/i1/tracks/" + trackId + "/streams?client_id=" + this.clientId);
+            var json = DynamicJson.Parse(str);
+
+            return json.http_mp3_128_url;
+        }
+
+        public bool DownloadMusicWithTrackId(Track track)
+        {
+            var fileName = ValidFileName(track.Title + ".mp3");
+
+            if (File.Exists("mp3/" + fileName))
+            {
+                // finish if the same file already exists
+                MessageBox.Show("download successfully finished.");
+                Environment.Exit(0);
+            }
+
+            var mp3Url = this.GetMp3Url(track.Id);
+
+            if (mp3Url == null)
+            {
+                return false;
+            }
+            else
+            {
+                new WebClient().DownloadFile(mp3Url, "mp3/" + fileName);
+                return true;
+            }
+        }
+    }
+}
---
+++ SoundCloudLikeDownloader/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using SoundCloud.NET;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SoundCloudLikeDownloader
+{
+    public partial class MainWindow : Window
+    {
+        private const string ClientId = "577cb16946b0657e1dbdbd1dc964ec18";
+        private const string ClientSecret = "a1104c50a0f7618e405dec4569f065bc";
+
+        private ObservableCollection<Like> _likeCollection = new ObservableCollection<Like>();
+        public ObservableCollection<Like> LikeCollection { get { return _likeCollection; } }
+
+        public class Like
+        {
+            public int Index { get; set; }
+            public string Title { get; set; }
+        }
+
+        public MainWindow()
+        {
+            if (!Directory.Exists("mp3"))
+            {
+                Directory.CreateDirectory("mp3");
+            }
+
+            InitializeComponent();
+        }
+
+        // Move Window
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var username = this.Username.Text;
+            var passwrod = this.Password.Password;
+
+            new Thread(() =>
+            {
+                var credentials = new SoundCloudCredentials(ClientId, ClientSecret, username, passwrod);
+                var client = new SoundCloudClient(credentials);
+                SoundCloudAccessToken token = null;
+
+                try
+                {
+                    token = client.Authenticate();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("login failed.");
+                    return;
+                }
+                var downloader = new Downloader(ClientId, token.AccessToken);
+
+                //fetch some data
+                if (client.IsAuthenticated)
+                {
+                    //Fetch current user info
+                    var mySelf = User.Me();
+                    var clientID = client.getClientID();
+                    var trackList = new List<Track>();
+
+                    for (var i = 0; i < mySelf.Favorites; i += 50)
+                    {
+                        var favorites = User.Me().GetFavorites(i);
+
+                        foreach (var favorite in favorites.Select((track, j) => new { track.Id, j }))
+                        {
+                            var track = Track.GetTrack(favorite.Id);
+                            if (downloader.DownloadMusicWithTrackId(track))
+                            {
+                                ListView1.Dispatcher.Invoke(
+                                    new Action(() =>
+                                    {
+                                        _likeCollection.Add(new Like() { Index = i + favorite.j, Title = track.Title });
+                                    }));
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("download successfully finished.");
+                }
+            }).Start();
+        }
+
+        // Exit Button
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
